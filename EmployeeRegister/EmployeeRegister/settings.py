638a664c26ad1dcd26af4f1852957e0ade212db5
Pratyush <pdihazarika35@gmail.com>
2022-07-03 22:10:50 +0530

    modified

--- EmployeeRegister/EmployeeRegister/settings.py
+++ EmployeeRegister/EmployeeRegister/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*-sp)5s6mch4g-&r)mk!_e&rc+#p6tzu()o1-#d)@@ogw12tsz'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'EmployeeDB',
         'USER':'postgres',
-        'PASSWORD':'postgres2@',
+        'PASSWORD':config('PASSWORD'),
         'HOST':'localhost',
 
     }
